2ed2a2a329b8c2c1eeb5dda0f00fa72dc7afb978
Miloš Nedeljković <milos@flocksocial.io>
2022-12-18 16:53:34 +0100

    user auth generate url and handle callback

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ client_id = os.environ["CLIENT_ID"]
 client_secret = os.environ["CLIENT_SECRET"]
 
 tt = Twitch(client_id=client_id, client_secret=client_secret, token_client=MemoryTokenClient())
-api = API(tt=tt)
-res = api.users.get_users(logins = ["negomir99"])
-print(res)
+# api = API(tt=tt)
+# res = api.users.get_users(logins = ["negomir99"])
+# print(res)
+
+print(tt.user_auth.generate_authorization_url(redirect_uri="http://localhost:5000/oauth/callback", scopes=["scope:one", "scope:two:three"]))
+print(tt.user_auth.handle_callback_url(""))
--- twitch/auth/user.py
+++ twitch/auth/user.py
@@ -1,4 +1,13 @@
+from urllib import parse
 from twitch.auth.token_store import TokenStore
+class AuthorizeInvalidCallbackURL(Exception):
+    pass
+
+class AuthorizeCallbackException(Exception):
+    pass
+
+class AuthorizeInvalidState(Exception):
+    pass
 
 class UserAuth:
     def __init__(self, client_id: str, token_store: TokenStore):
@@ -14,3 +23,41 @@ class UserAuth:
 
     def save_token(self, token: str):
         self.token_store.save(id=self.client_id, token=token)
+
+    def generate_authorization_url(self, redirect_uri: str, scopes: list) -> str:
+        params = {
+            "client_id": self.client_id,
+            "redirect_uri": redirect_uri,
+            "scope": "+".join(scopes),
+            "state": "state_check"
+        }
+
+        return "https://id.twitch.tv/oauth2/authorize?" + parse.urlencode(params)
+
+    def handle_callback_url(self, callback_url: str) -> str:
+        try:
+            query = callback_url.split("?")[1]
+            kvs = query.split("&")
+            data = {}
+            for kv in kvs:
+                key_val = kv.split("=")
+                val = ""
+                if len(key_val) == 2:
+                    val = key_val[1]
+                data[key_val[0]]=val
+        except Exception as ex:
+            raise AuthorizeInvalidCallbackURL("invalid_url", ex)
+
+        if "state" not in data:
+            raise AuthorizeInvalidState("state_error", "state parameter is missing")
+
+        if data["state"] != "state_check":
+            raise AuthorizeInvalidState("state_error", "state parameter does not match")
+
+        if "error" in data:
+            raise AuthorizeCallbackException(data["error"], data["error_description"])
+
+        if "code" not in data:
+            raise AuthorizeCallbackException("code_error", "code parameter is missing")
+
+        return data["code"]
