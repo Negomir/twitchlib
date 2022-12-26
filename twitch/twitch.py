a1e149b1b9e78fed75e87b96d399fb1001ed81ba
Miloš Nedeljković <milos@flocksocial.io>
2022-12-26 14:03:10 +0100

    pass redirect_uri to user_auth

--- twitch/twitch.py
+++ twitch/twitch.py
@@ -3,8 +3,8 @@ from twitch.auth.app import AppAuth
 from twitch.auth.user import UserAuth
 
 class Twitch:
-    def __init__(self, client_id: str, client_secret: str, token_client: TokenStoreClient):
+    def __init__(self, client_id: str, client_secret: str, redirect_uri: str, token_client: TokenStoreClient):
         self.client_id = client_id
         self.client_secret = client_secret
         self.app_auth = AppAuth(client_id, client_secret, TokenStore(token_client, "app"))
-        self.user_auth = UserAuth(client_id, client_secret, TokenStore(token_client, "user"))
+        self.user_auth = UserAuth(client_id, client_secret, redirect_uri, TokenStore(token_client, "user"))
